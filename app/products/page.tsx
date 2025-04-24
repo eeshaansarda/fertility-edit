818a0d1c19225b2377ed9f4233b5778525734cc3
Eeshaan Sarda <eeshaansarda98@gmail.com>
2025-04-25 00:00:00 +0530

    corrected eslint and dynamic fetch errors

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,20 +1,14 @@
 import ProductCard from "@/app/components/ProductCard";
 import { Product } from "@/lib/generated/prisma";
 
+export const dynamic = 'force-dynamic'
+
 const ProductsPage = async () => {
-  const origin = process.env.BASE_URL ?? 'http://localhost:3000'
-  const response = await fetch(`${origin}/api/products`)
+  const origin = process.env.BASE_URL ?? 'http://localhost:3000';
+  const response = await fetch(`${origin}/api/products`);
   const products: Product[] = await response.json();
-  console.log(products);
   return (
     <div className="p-8">
-      {/*
-      <CategoryCards
-        selectedCategory={undefined}
-        onSelectCategory={() => {}}
-      />
-*/}
-
       <div className="flex flex-col gap-4">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "lib/generated/**"
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
